de2893bf04b4e838692d8a9a015941ed579ad410
RRCTools <deepanshsoni@rrccompanies.com>
2026-03-17 16:32:35 -0400

    Test 16

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.set_page_config(
     page_title="RRC Loss Report Calculator",
-    page_icon="⚡",
+    page_icon="⚡⚡",
     layout="wide",
     initial_sidebar_state="expanded",
 )
