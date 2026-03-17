76079a445d59dc4a55334e0490e2ed07a17f2a16
RRCTools <deepanshsoni@rrccompanies.com>
2026-03-17 16:34:01 -0400

    test17

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(
-    page_title="RRC Loss Report Calculator",
+    page_title="RRC EE Loss Report Calculator",
     page_icon="⚡⚡",
     layout="wide",
     initial_sidebar_state="expanded",
